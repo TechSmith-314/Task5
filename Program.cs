be99f9677d471cd530177a24b5991e020d016e11
Евгений Пустовалов <techsmith314@hotmail.com>
2023-01-24 20:33:00 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Иванов";
+            string lastName = "Иван";
+
+            Console.WriteLine($"Имя: {firstName}\nФамилия: {lastName}");
+
+            string x = firstName;
+            firstName = lastName;
+            lastName = x;
+
+            Console.WriteLine($"Имя: {firstName}\nФамилия: {lastName}");
+        }
+    }
+}
